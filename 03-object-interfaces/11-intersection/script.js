6af0cd8a288d79b53c1497565cacddb25b1f8798
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-12-07 08:24:29 -0300

    intersecoes

---
+++ 03-object-interfaces/11-intersection/script.js
@@ -0,0 +1,24 @@
+"use strict";
+//Intersection
+function handleProdutoCarro(dados) {
+    dados.rodas;
+    dados.portas;
+    dados.preco;
+}
+handleProdutoCarro({
+    preco: 20000,
+    rodas: 4,
+    portas: 5,
+});
+const dado1 = {
+    preco: 20000,
+    rodas: 4,
+    portas: 5,
+};
+const dado2 = {
+    preco: 20000,
+    rodas: 4,
+    portas: 5,
+};
+window.userId = 200;
+console.log(window.userId);
